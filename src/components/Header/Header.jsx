2c168a150271393eb0b5fc82d0a8582035cac8ff
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-05-13 01:33:52 +0600

    responsive complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-white" // Added text-white class here
+                className="h-5 w-5 text-white" 
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -24,7 +24,7 @@ const Header = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-red-400 md:text-black md:bg-orange-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black-100 rounded-box w-52 text-red-400  "
             >
               <li className="text-white">
                 <a>Dashboard </a>
